0e5867b191187b11ed174440588185aabf4dc286
alumMA <anajuriesp1@educacion.navarra.es>
2024-11-19 11:31:25 +0100

    CAMBIO CARPETA PHP, AÑADIDO PIE.PHP

--- PHP/consejos_mecanicos.php
+++ PHP/consejos_mecanicos.php
@@ -209,10 +209,6 @@
 
         </div>
     </div>
-    <div class="pie_pagina">
-        <p class="texto_pie">© 2024 Forocoches. Todos los derechos reservados.</p>
-        <div class="pie_enlaces_interes">
-            <img src="../imagenes/logo-search-grid-1x.png" alt=""> | <img src="../imagenes/logo.png" alt="">
-        </div>
-    </div>
-</body>
+<?php
+    include "pie.php";
+?>
---
+++ PHP/pie.php
@@ -0,0 +1,7 @@
+<div class="pie_pagina">
+        <p class="texto_pie">© 2024 Forocoches. Todos los derechos reservados.</p>
+        <div class="pie_enlaces_interes">
+            <img src="../imagenes/logo-search-grid-1x.png" alt=""> | <img src="../imagenes/logo.png" alt="">
+        </div>
+    </div>
+</body>
