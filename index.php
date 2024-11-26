dacf9e39378052c6c5ec7c199f7294bd6e5ede59
alumMA <ugallarfer@educacion.navarra.es>
2024-11-26 10:47:13 +0100

    Cabecera y pie de index

--- index.php
+++ index.php
@@ -1,49 +1,59 @@
 <!DOCTYPE html>
-<html lang="es">
+<html>
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Diseño de Página</title>
-    <link rel="stylesheet" href="estilos.css">
+    <meta charset='utf-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <title>AutoSelecto</title>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <link rel='stylesheet' type='text/css' media='screen' href='css/cabecera.css'>
+    <link rel='stylesheet' type='text/css' media='screen' href='css/pie_de_pagina.css'>
+    <link rel='stylesheet' type='text/css' media='screen' href='css/index.css'>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+    <link rel="shortcut icon" href="../imagenes/logo-search-grid-1x.png" type="image/x-icon">
+    <script src='main.js'></script>
 </head>
+
 <body>
-    <header class="cabecera">
-        Cabecera
-    </header>
+    <div class="cabecera">
+        <div class="menu">
+            <div class="menu_buttons">
+                <a href="index.html">Indice</a>
+                <a href="contenido_general.html">Contenidos generales</a>
+                <div class="dropdown">
+                    <a>Ofertas destacadas<i class="material-icons">expand_more</i></a>
+                    <div class="dropdown_content">
+                        <a href="../html/ofertas_destacadas_ambas.html">Todos los vehiculos</a>
+                        <a href="../html/ofertas_destacadas_coches.html">Coches</a>
+                        <a href="../html/ofertas_destacadas_motos.html">Motos</a>
+                    </div>
+                </div>
+                <div class="dropdown">
+                    <a>Últimas noticias<i class="material-icons">expand_more</i></a>
+                    <div class="dropdown_content">
+                        <a href="../html/coche_electrico.html">Coches eléctricos</a>
+                        <a href="../html/coche_motor.html">Coches de combustión</a>
+                        <a href="../html/.html">Motocicletas y ciclomotores</a>
+                    </div>
+                </div>
+                <div class="dropdown">
+                    <a href="../html/consejos_mecanicos.html">Consejos mecánicos</a>
+                    <a href="../html/Contacto.html">Contacto</a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="body">
+        
+    </div>
 
-    <main>
-        <section class="seccion1">
-            <div class="foto">
-                Foto con nombre
-            </div>
-        </section>
 
-        <section class="seccion2">
-            <div class="item">
-                <a href="https://ejemplo1.com">Enlace 1</a>
-            </div>
-            <div class="item">
-                <a href="https://ejemplo2.com">Enlace 2</a>
-            </div>
-            <div class="item">
-                <a href="https://ejemplo3.com">Enlace 3</a>
-            </div>
-        </section>
 
-        <section class="seccion3">
-            <div class="imagen-texto">
-                <a href="https://ejemplo4.com">Imagen</a>
-                <p>Texto relacionado</p>
-            </div>
-            <div class="imagen-texto">
-                <a href="https://ejemplo5.com">Imagen</a>
-                <p>Texto relacionado</p>
-            </div>
-        </section>
-    </main>
 
-    <footer class="pie">
-        Pie de página
-    </footer>
+    <div class="pie_pagina">
+        <p class="texto_pie">© 2024 Forocoches. Todos los derechos reservados.</p>
+        <div class="pie_enlaces_interes">
+            <img src="../imagenes/logo-search-grid-1x.png" alt=""> | <img src="../imagenes/logo.png" alt="">
+        </div>
+    </div>
 </body>
-</html>
