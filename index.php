ce77413627919bd7cfba1b83bbecb0ed7be37dca
UnaiGallardon <ugallarfer@educacion.navarra.es>
2024-12-27 20:50:20 +0100

    revisar y faltan enlaces a otras páginas

--- index.php
+++ index.php
@@ -1,49 +1,90 @@
 <!DOCTYPE html>
-<html lang="es">
+<html>
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Diseño de Página</title>
-    <link rel="stylesheet" href="estilos.css">
+    <meta charset='utf-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <title>AutoSelecto</title>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <link rel='stylesheet' type='text/css' media='screen' href='css/index.css'>
+    <link rel='stylesheet' type='text/css' media='screen' href='css/cabecera1.css'>
+    <link rel='stylesheet' type='text/css' media='screen' href='css/pie_de_pagina.css'>
+    <link rel='stylesheet' type='text/css' media='screen' href='css/consejos_mecanicoss.css'>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+    <link rel="shortcut icon" href="../imagenes/logo-search-grid-1x.png" type="image/x-icon">
+    <script src='main.js'></script>
 </head>
+
 <body>
-    <header class="cabecera">
-        Cabecera
-    </header>
+    <div class="cabecera">
+        <div class="menu">
+            <div class="menu_buttons">
+                <a href="index.html">Indice</a>
+                <a href="contenido_general.html">Contenidos generales</a>
+                <div class="dropdown">
+                    <a>Ofertas destacadas<i class="material-icons">expand_more</i></a>
+                    <div class="dropdown_content">
+                        <a href="html/ofertas_destacadas_ambas.html">Todos los vehiculos</a>
+                        <a href="html/ofertas_destacadas_coches.html">Coches</a>
+                        <a href="html/ofertas_destacadas_motos.html">Motos</a>
+                    </div>
+                </div>
+                <div class="dropdown">
+                    <a>Últimas noticias<i class="material-icons">expand_more</i></a>
+                    <div class="dropdown_content">
+                        <a href="html/coche_electrico.html">Coches eléctricos</a>
+                        <a href="html/coche_motor.html">Coches de combustión</a>
+                        <a href="html/.html">Motocicletas y ciclomotores</a>
+                    </div>
+                </div>
+                <div class="dropdown">
+                    <a href="PHP/consejos_mecanicos.php">Consejos mecánicos</a>
+                    <a href="PHP/Contacto.html">Contacto</a>
+                </div>
+            </div>
+        </div>
+    </div>
 
     <main>
-        <section class="seccion1">
+        <div class="seccion1">
             <div class="foto">
-                Foto con nombre
+                <img src="imagenes/portada.jpg" alt="bmw portada" />
+                <h2>Autoselecto</h2>
             </div>
-        </section>
+        </div>
 
-        <section class="seccion2">
+        <div class="seccion2">
             <div class="item">
-                <a href="https://ejemplo1.com">Enlace 1</a>
+                <a href="#"><img src="imagenes/contenidos.jpg" alt="imagen contenidos generales">
+                <p>Contenidos generales</p></a>
             </div>
             <div class="item">
-                <a href="https://ejemplo2.com">Enlace 2</a>
+                <a href="#"><img src="imagenes/ultimas.jpg" alt="Imagen últimas noticias">
+                <p>Últimas noticias</p></a>
             </div>
             <div class="item">
-                <a href="https://ejemplo3.com">Enlace 3</a>
+                <a href="PHP/consejos_mecanicos.php"><img src="imagenes/seguro_vida.jpg" alt="imagen consejos mecánicos">
+                <p>Consejos mecánicos</p></a>
             </div>
-        </section>
+        </div>
 
-        <section class="seccion3">
+        <div class="seccion3">
+        <a href="#">
             <div class="imagen-texto">
-                <a href="https://ejemplo4.com">Imagen</a>
-                <p>Texto relacionado</p>
+                <img src="imagenes/ofertas_coches_septiembre.jpg" alt="imagen ofertas">
+                <p>Ofertas destacadas</p>
             </div>
-            <div class="imagen-texto">
-                <a href="https://ejemplo5.com">Imagen</a>
-                <p>Texto relacionado</p>
-            </div>
-        </section>
+        </a>
+
+        </div>
     </main>
 
-    <footer class="pie">
-        Pie de página
-    </footer>
+    <div class="pie_pagina">
+        <p class="texto_pie">© 2024 Forocoches. Todos los derechos reservados.</p>
+        <div class="pie_enlaces_interes">
+            <img src="imagenes/logo-search-grid-1x.png" alt=""> | <img src="imagenes/logo.png" alt="">
+        </div>
+    </div>
+</body>
 </body>
 </html>
