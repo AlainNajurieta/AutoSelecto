6050fed991c368ff302e7efc217706f51a724fa6
UnaiGallardon <ugallarfer@educacion.navarra.es>
2024-12-28 16:38:50 +0100

    .

--- index.php
+++ index.php
@@ -1,15 +1,48 @@
 <!DOCTYPE html>
-<html lang="es">
+<html>
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Diseño de Página</title>
-    <link rel="stylesheet" href="estilos.css">
+    <meta charset='utf-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <title>AutoSelecto</title>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <link rel='stylesheet' type='text/css' media='screen' href='css/index.css'>
+    <link rel='stylesheet' type='text/css' media='screen' href='css/cabecera.css'>
+    <link rel='stylesheet' type='text/css' media='screen' href='css/pie_de_pagina.css'>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+    <link rel="shortcut icon" href="../imagenes/logo-search-grid-1x.png" type="image/x-icon">
+    <script src='main.js'></script>
 </head>
+
 <body>
-    <header class="cabecera">
-        Cabecera
-    </header>
+    <div class="cabecera">
+        <div class="menu">
+            <div class="menu_buttons">
+                <a href="index.html">Indice</a>
+                <a href="contenido_general.html">Contenidos generales</a>
+                <div class="dropdown">
+                    <a>Ofertas destacadas<i class="material-icons">expand_more</i></a>
+                    <div class="dropdown_content">
+                        <a href="../html/ofertas_destacadas_ambas.html">Todos los vehiculos</a>
+                        <a href="../html/ofertas_destacadas_coches.html">Coches</a>
+                        <a href="../html/ofertas_destacadas_motos.html">Motos</a>
+                    </div>
+                </div>
+                <div class="dropdown">
+                    <a>Últimas noticias<i class="material-icons">expand_more</i></a>
+                    <div class="dropdown_content">
+                        <a href="../html/coche_electrico.html">Coches eléctricos</a>
+                        <a href="../html/coche_motor.html">Coches de combustión</a>
+                        <a href="../html/.html">Motocicletas y ciclomotores</a>
+                    </div>
+                </div>
+                <div class="dropdown">
+                    <a href="../html/consejos_mecanicos.html">Consejos mecánicos</a>
+                    <a href="../html/Contacto.html">Contacto</a>
+                </div>
+            </div>
+        </div>
+    </div>
 
     <main>
         <section class="seccion1">
@@ -20,12 +53,15 @@
 
         <section class="seccion2">
             <div class="item">
+                <img src="" alt="">
                 <a href="https://ejemplo1.com">Enlace 1</a>
             </div>
             <div class="item">
+                <img src="" alt="">
                 <a href="https://ejemplo2.com">Enlace 2</a>
             </div>
             <div class="item">
+                <img src="" alt="">
                 <a href="https://ejemplo3.com">Enlace 3</a>
             </div>
         </section>
