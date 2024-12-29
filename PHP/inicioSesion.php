295b9784f413c7c0faf245eab0fe193664c8982e
UnaiGallardon <ugallarfer@educacion.navarra.es>
2024-12-29 18:47:02 +0100

    formulario inicio sesión

---
+++ PHP/inicioSesion.php
@@ -0,0 +1,25 @@
+<?php
+    include "cabecera.php";
+?>
+
+    <div class="container">
+        <form action="#">
+            <h2>Iniciar sesión</h2>
+            <div class="form">
+                <input type="text" placeholder="Usuario" required>
+            </div>
+            <div class="form">
+                <input type="password" placeholder="Contraseña" required>
+            </div>
+            <div class="form">
+                <button type="button">Registrarse</button>
+            </div>
+            <div class="form">
+                <button type="submit">Siguiente</button>
+            </div>
+        </form>
+    </div>
+
+<?php
+    include "pie.php";
+?>
