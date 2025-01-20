20d843bfeec5e1076536da0073563abde06c74aa
alumMA <anajuriesp1@educacion.navarra.es>
2025-01-20 09:13:01 +0100

    Cambios rutas controlador

--- PHP/index.php
+++ PHP/index.php
@@ -1,12 +1,37 @@
 <?php 
 
-//if ($_GET["page"] = "consejos_mecanicos"){
-//    include 'consejos_mecanicos.php';
+if (isset($_GET['page'])) {
+    $page = $_GET['page'];
+    if ($page === 'principal.php') {
+        include 'principal.php';
+    } elseif ($page === 'consejos_mecanicos.php') {
+        include 'consejos_mecanicos.php';
+    } elseif ($page === 'contacto.php') {
+        include 'contacto.php';
+    } elseif ($page === 'contenido_general.php') {
+        include 'contenido_general.php';
+    } elseif ($page === 'ofertas_destacadas_ambas.php') {
+        include 'ofertas_destacadas_ambas.php';
+    } elseif ($page === 'ofertas_destacadas_coches.php') {
+        include 'ofertas_destacadas_coches.php';
+    } elseif ($page === 'ofertas_destacadas_motos.php') {
+        include 'ofertas_destacadas_motos.php';
+    } else {
+        include 'error.php'; // Página para manejar casos no válidos
+    }
+} else {
+    include 'inicioSesion.php'; // Página predeterminada
+}
+
+
+
+
+
+
+
+
+
+//if(isset($_POST["enviar"])){
+//    include "index.php";
 //}
-
-    if(isset($_POST["enviar"])){
-        include "index.php";
-    } 
-
-
 ?>
--- PHP/navegador.php
+++ PHP/navegador.php
@@ -1,24 +1,21 @@
-
 </head>
-
 <body>
     <div class="cabecera">
         <div class="menu">
             <div class="menu_buttons">
-                <a href="controlador.php?page='index.php'">Indice</a>
-                <a href="controlador.php?page='contenido_general.php'">Contenidos generales</a>
+                <a href="index.php?page=principal.php">Indice</a>
+                <a href="index.php?page=contenido_general.php">Contenidos generales</a>
                 <div class="dropdown">
                     <a>Ofertas destacadas<i class="material-icons">expand_more</i></a>
                     <div class="dropdown_content">
-                        <a href="controlador.php?page='ofertas_destacadas_ambas.php'">Todos los vehiculos</a>
-                        <a href="controlador.php?page='ofertas_destacadas_coches.php'">Coches</a>
-                        <a href="controlador.php?page='ofertas_destacadas_motos.php'">Motos</a>
+                        <a href="index.php?page=ofertas_destacadas_ambas.php">Todos los vehículos</a>
+                        <a href="index.php?page=ofertas_destacadas_coches.php">Coches</a>
+                        <a href="index.php?page=ofertas_destacadas_motos.php">Motos</a>
                     </div>
                 </div>
-                <div class="dropdown">
-                    <a href="controlador.php?page='consejos_mecanicos.php'">Consejos mecánicos</a>
-                    <a href="controlador.php?page='contacto.php'">Contacto</a>
-                </div>
+                <a href="index.php?page=consejos_mecanicos.php">Consejos mecánicos</a>
+                <a href="index.php?page=contacto.php">Contacto</a>
             </div>
         </div>
     </div>
+
