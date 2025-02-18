fe56c14f78d37e3a24479266bfffc116391762d2
UnaiGallardon <ugallarfer@educacion.navarra.es>
2025-02-18 19:19:43 +0100

    Comenzada validacion de formulario de contacto y comenzada la página de venta de coches

---
+++ PHP/coches.php
@@ -0,0 +1,31 @@
+<?php 
+    include "cabecera.php";
+?>
+    <link rel="stylesheet" href="../CSS/coches.css">
+<?php 
+    include "navegador.php";
+?> 
+<body>
+    <header>
+        <h1>Coches</h1>
+    </header>
+
+    <main>
+        <section class="services">
+            <div class="service-card">
+                <img src="../imagenes/ultimas.jpg" alt="Lavado de Servicio Completo">
+                <p class="price">$44</p>
+                <p class="description">Sample text. Click to select the text box. Click again or double click to start editing the text.</p>
+                <a href="#" class="button">MÁS INFORMACIÓN</a>
+                <a href="#" class="buy-button">COMPRAR</a> 
+            </div>
+
+
+        </section>
+    </main>
+
+    <?php 
+        include "pie.php";
+    ?>
+    </body>
+</html>
